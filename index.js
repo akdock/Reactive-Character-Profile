4d96da2c256b6cd42df2810286556b42171c258f
nn <soyeon20123@gmail.com>
2025-03-05 15:03:33 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
-// //You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-// import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+// 필요한 모듈 가져오기
+import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+import { saveSettingsDebounced } from "../../../../script.js";
 
-// //You'll likely need to import some other functions from the main script
-// import { saveSettingsDebounced } from "../../../../script.js";
+// 확장 이름 및 경로 설정
+const extensionName = "Reactive Character Profile";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {};
 
-// // Keep track of where your extension is located, name should match repo name
-// const extensionName = "Reactive Character Profile";
-// const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-// const extensionSettings = extension_settings[extensionName];
-// const defaultSettings = {};
- 
-// // Loads the extension settings if they exist, otherwise initializes them to the defaults.
-// async function loadSettings() {
-//   //Create the settings if they don't exist
-//   extension_settings[extensionName] = extension_settings[extensionName] || {};
-//   if (Object.keys(extension_settings[extensionName]).length === 0) {
-//     Object.assign(extension_settings[extensionName], defaultSettings);
-//   }
+// 설정을 불러오거나 기본값으로 초기화
+async function loadSettings() {
+  extension_settings[extensionName] = extension_settings[extensionName] || {};
+  if (Object.keys(extension_settings[extensionName]).length === 0) {
+    Object.assign(extension_settings[extensionName], defaultSettings);
+  }
 
-//   // Updating settings in the UI
-//   $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
-// }
+  $("#use_main_api").prop("checked", extension_settings[extensionName].use_main_api).trigger("input");
+  $("#llm_provider").val(extension_settings[extensionName].llm_provider).trigger("change");
+  $("#llm_model").val(extension_settings[extensionName].llm_model).trigger("change");
+  $("#update_frequency").val(extension_settings[extensionName].update_frequency).trigger("input");
+}
 
-// // This function is called when the extension settings are changed in the UI
-// function onExampleInput(event) {
-//   const value = Boolean($(event.target).prop("checked"));
-//   extension_settings[extensionName].example_setting = value;
-//   saveSettingsDebounced();
-// }
+// 메인 API 체크박스 변경 시
+function onMainApiToggle(event) {
+  const value = Boolean($(event.target).prop("checked"));
+  extension_settings[extensionName].use_main_api = value;
+  $("#llm_provider, #llm_model, .parameter-settings input").prop("disabled", value);
+  saveSettingsDebounced();
+}
 
-// // This function is called when the button is clicked
-// function onButtonClick() {
-//   // You can do whatever you want here
-//   // Let's make a popup appear with the checked setting
-//   toastr.info(
-//     `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-//     "A popup appeared because you clicked the button!"
-//   );
-// }
+// LLM 제공자 변경 시
+function onProviderChange(event) {
+  const value = $(event.target).val();
+  extension_settings[extensionName].llm_provider = value;
+  saveSettingsDebounced();
+}
 
-// // This function is called when the extension is loaded
-// jQuery(async () => {
-//   // This is an example of loading HTML from a file
-//   const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+// LLM 모델 변경 시
+function onModelChange(event) {
+  const value = $(event.target).val();
+  extension_settings[extensionName].llm_model = value;
+  saveSettingsDebounced();
+}
 
-//   // Append settingsHtml to extensions_settings
-//   // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-//   // Left should be extensions that deal with system functions and right should be visual/UI related 
-//   $("#extensions_settings").append(settingsHtml);
+// 업데이트 주기 변경 시
+function onUpdateFrequencyChange(event) {
+  const value = parseFloat($(event.target).val());
+  extension_settings[extensionName].update_frequency = value;
+  saveSettingsDebounced();
+}
 
-//   // These are examples of listening for events
-//   $("#my_button").on("click", onButtonClick);
-//   $("#example_setting").on("input", onExampleInput);
+// 확장 로드 시 실행
+jQuery(async () => {
+  const settingsHtml = await $.get(`${extensionFolderPath}/drawer.html`);
+  $("#extensions_settings").append(settingsHtml);
 
-//   // Load settings when starting things up (if you have any)
-//   loadSettings();
-// });
+  // 이벤트 리스너 추가
+  $("#use_main_api").on("input", onMainApiToggle);
+  $("#llm_provider").on("change", onProviderChange);
+  $("#llm_model").on("change", onModelChange);
+  $("#update_frequency").on("input", onUpdateFrequencyChange);
+
+  // 설정 불러오기
+  loadSettings();
+});
 
 // 전역 변수: stat 색상은 최초 API 호출 시 결정되어 이후에도 동일하게 사용
 let fixedStatColor = null;
